fe6e69d3602551488a1629689b19a97742321daf
Philipp Gabler <phips.gabler@gmail.com>
2015-11-09 22:39:53 +0100

    Change testing library to Hspec.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE TemplateHaskell, OverloadedStrings #-}
 
---import Test.QuickCheck
-import Test.HUnit
+import Test.Hspec
+import Test.QuickCheck
+--import Test.HUnit
+import qualified Data.Map.Strict as M
+
 import MiniLambda
 import MiniLambda.Parser
 
@@ -9,33 +12,22 @@ cons = lambda "x" <.> lambda "y" <.> lambda "f" <.> "f" @@ "x" @@ "y"
 car = lambda "t" <.> "t" @@ (lambda "x" <.> lambda "_" <.> "x")
 cdr = lambda "t" <.> "t" @@ (lambda "_" <.> lambda "y" <.> "y")
 
-testTuples1 = TestCase $ do
-  let t = (cons @@ "1" @@ "2")
-  assertEqual
-    "The car of (1,2) is 1"
-    (normalize $ car @@ t)
-    "1"
-  assertEqual
-    "The cdr of (1,2) is 2"
-    (normalize $ cdr @@ t)
-    "2"
-    
-testTuples2 = TestCase $ assertEqual
-  "car/cons invariant is kept"
-  (normalize $ lambda "x" <.> lambda "y" <.> car @@ (cons @@ "x" @@ "y"))
-  (lambda "x" <.> lambda "y" <.> "x")
-
-etaEquivalence = TestCase $ assertEqual
-  "Eta-minimality"
-  (normalize $ lambda "x" <.> "f" @@ "x")
-  ("f")
-
-
--- testParserBasic = TestCase $ do
---   assertEqual
-
-main = runTestTT $ TestList [ testTuples1
-                            , testTuples2
-                            , etaEquivalence
-                            --, testParserBasic
-                            ]
+main = hspec $ do
+  describe "MiniLambda" $ do
+    describe "normalize" $ do
+      let t = (cons @@ "1" @@ "2")
+      it "can reduce car" $ do
+        (normalize $ car @@ t) `shouldBe` "1"
+      it "can reduce cdr" $ do
+        (normalize $ cdr @@ t) `shouldBe` "2"
+      it "performs eta reductions" $ do
+        (normalize $ lambda "x" <.> "f" @@ "x") `shouldBe` "f"
+    describe "normalizeWith" $ do
+      let testPrelude = M.fromList [("cons", cons)
+                                  , ("car", car)
+                                  , ("cdr", cdr)
+                                  ]
+      it "keeps the car/cons invariant" $ do
+        (normalizeWith testPrelude $ (car @@ (cons @@ "x" @@ "y"))) `shouldBe` "x"
+      it "keeps the cdr/cons invariant" $ do
+        (normalizeWith testPrelude $ (cdr @@ (cons @@ "x" @@ "y"))) `shouldBe` "y"
